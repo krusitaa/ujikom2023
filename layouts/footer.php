856e7545b066b8099c1fa93a22d8a54bfce6a29b
krusitaa <127715364+krusitaa@users.noreply.github.com>
2023-03-14 10:10:00 +0700

    Add files via upload

---
+++ layouts/footer.php
@@ -0,0 +1,10 @@
+<footer class="fixed - buttom" style="background-color: #D8BFD8; ">
+  <div class="container">
+    <p class="text-center">UKK RPL 2023 | Raespa krusita putri | SMK MAHARDIKA</p>
+  </div>
+
+</footer>
+
+<script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
+</body>
+</html>
